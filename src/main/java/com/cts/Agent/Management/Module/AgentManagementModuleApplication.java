c3066d1d5547a029f64e621f68c4a4a751d30057
babuprasadravi <babuprasad784@gmail.com>
2025-05-11 19:51:05 +0530

    first commit

---
+++ src/main/java/com/cts/Agent/Management/Module/AgentManagementModuleApplication.java
@@ -0,0 +1,15 @@
+package com.cts.Agent.Management.Module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentManagementModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentManagementModuleApplication.class, args);
+
+		
+	}
+
+}
---
+++ src/test/java/com/cts/Agent/Management/Module/AgentManagementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.Agent.Management.Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentManagementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
