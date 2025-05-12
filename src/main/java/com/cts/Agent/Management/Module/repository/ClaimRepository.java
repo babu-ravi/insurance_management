6ed3a20902f95c04efaf1b139ea4bc2940890456
Ravi <2400607@cognizant.com>
2025-05-12 17:29:28 +0530

    Optional Filtering Implemented

--- src/main/java/com/cts/Agent/Management/Module/repository/ClaimRepository.java
+++ src/main/java/com/cts/Agent/Management/Module/repository/ClaimRepository.java
@@ -1,8 +1,12 @@
 package com.cts.Agent.Management.Module.repository;
 
 import com.cts.Agent.Management.Module.model.Claim;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-public interface ClaimRepository extends JpaRepository <Claim, Long> {
-
+@Repository
+public interface ClaimRepository extends JpaRepository<Claim, Long>, JpaSpecificationExecutor<Claim> {
 }
